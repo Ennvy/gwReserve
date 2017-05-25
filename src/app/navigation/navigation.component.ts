ec2bda88fcac1642f4c4423de085e36952501c19
Josh R. Dunlavy <jdunlavy@gmail.com>
2017-05-25 16:08:08 -0700

    feature: add navigation to app component

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-navigation',
+	selector: 'gw-navigation',
 	templateUrl: './navigation.component.html',
 	styleUrls: ['./navigation.component.css']
 })
