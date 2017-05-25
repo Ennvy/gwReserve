bbfcdfd962b044e2602d4b28796065e72bd4f6fd
Josh R. Dunlavy <jdunlavy@gmail.com>
2017-05-25 01:58:54 -0700

    feature: import app routing module and routed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,55 +1,24 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router"
+
+import { AppRoutingModule, routedComponents } from "./app.routing.module";
 
 import { AppComponent } from "./app.component";
-import { ExercisesComponent } from "./exercises/exercises.component";
-import { LoginComponent } from "./login/login.component";
 import { NavigationComponent } from "./navigation/navigation.component";
-import { RoomsComponent } from "./rooms/rooms.component";
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		ExercisesComponent,
-		LoginComponent,
 		NavigationComponent,
-		RoomsComponent
+		routedComponents
 	],
 	imports: [
 		BrowserModule,
 		FormsModule,
-		RouterModule.forRoot([
-			// let's add our "root" route
-			{
-				path: "",
-				component: LoginComponent
-			},
-
-			// add specific routes
-			{
-				path: "exercises",
-				component: ExercisesComponent
-			},
-			{
-				path: "login",
-				component: LoginComponent
-			},
-			{
-				path: "rooms/:id",
-				component: RoomsComponent
-			},
-
-			// finally, add "catch-all" route, taking the place of "otherwise" from legacy Angular
-			{
-				path: "**",
-				pathMatch: "full",
-				redirectTo: "login"
-			}
-		])
+		AppRoutingModule
 	],
 	providers: [],
-	bootstrap: [AppComponent]
+	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
