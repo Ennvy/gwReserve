4f1dee802ceb519a6ce564770501600c2d2108d2
Josh R. Dunlavy <jdunlavy@gmail.com>
2017-05-25 01:46:22 -0700

    feature(room component, route params): implement route params
    This includes both snapshot and observable

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/map";
 
 @Component({
 	selector: "app-rooms",
@@ -6,7 +10,31 @@ import { Component, OnInit } from "@angular/core";
 	styleUrls: ["./rooms.component.css"]
 })
 export class RoomsComponent implements OnInit {
-	constructor() { }
+	public id: number;
 
-	ngOnInit() { }
+	// ActivatedRoute is provided by RouterModule
+	constructor(private route: ActivatedRoute) { }
+
+	ngOnInit() {
+		console.log("ActivatedRoute snapshot", this.route.snapshot.params);
+
+		// get a snapshot
+		this.id = parseInt(this.route.snapshot.params["id"]);
+
+		console.log("Room ID is ", this.id, " via snapshot");
+
+		// deploy an observable
+		this.route.params
+			.map(params => {
+				console.log("Before map", params);
+				return params["id"];
+			})
+			.do(params => {
+				console.log("After map", params);
+			})
+			.subscribe(id => {
+				this.id = parseInt(id);
+				console.log("Room ID is ", this.id, " via subscribe");
+			});
+	}
 }
