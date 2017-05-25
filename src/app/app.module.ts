ce16b5e033bfb36fae544dc5be823b53638bb586
Josh R. Dunlavy <jdunlavy@gmail.com>
2017-05-25 01:18:53 -0700

    feature(stupid simple routing): implement very basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router"
 
 import { AppComponent } from "./app.component";
 import { ExercisesComponent } from "./exercises/exercises.component";
@@ -18,7 +19,35 @@ import { RoomsComponent } from "./rooms/rooms.component";
 	],
 	imports: [
 		BrowserModule,
-		FormsModule
+		FormsModule,
+		RouterModule.forRoot([
+			// let's add our "root" route
+			{
+				path: "",
+				component: LoginComponent
+			},
+
+			// add specific routes
+			{
+				path: "exercises",
+				component: ExercisesComponent
+			},
+			{
+				path: "login",
+				component: LoginComponent
+			},
+			{
+				path: "rooms/:id",
+				component: RoomsComponent
+			},
+
+			// finally, add "catch-all" route, taking the place of "otherwise" from legacy Angular
+			{
+				path: "**",
+				pathMatch: "full",
+				redirectTo: "login"
+			}
+		])
 	],
 	providers: [],
 	bootstrap: [AppComponent]
