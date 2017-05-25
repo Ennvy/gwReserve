c6b92959e38b174e92f33e9f2b0a687490b2e463
Josh R. Dunlavy <jdunlavy@gmail.com>
2017-05-25 01:20:46 -0700

    style: ' to "

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-	selector: 'app-rooms',
-	templateUrl: './rooms.component.html',
-	styleUrls: ['./rooms.component.css']
+	selector: "app-rooms",
+	templateUrl: "./rooms.component.html",
+	styleUrls: ["./rooms.component.css"]
 })
 export class RoomsComponent implements OnInit {
-
 	constructor() { }
 
 	ngOnInit() { }
