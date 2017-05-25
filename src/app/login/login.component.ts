fbecac56220002963d340912d887106fff0307c4
Josh R. Dunlavy <jdunlavy@gmail.com>
2017-05-25 03:10:14 -0700

    feature: import login service, inject it, implement login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService, IUser } from "./../services/login-service.service";
 
 @Component({
 	selector: 'app-login',
@@ -6,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-	constructor() { }
+	public loggedInUser:IUser;
 
-	ngOnInit() { }
+	constructor(private loginService:LoginService) { }
+
+	ngOnInit() {
+		this.init();
+	}
+
+	login() {
+		this.loginService.login();
+		this.init();
+	}
+
+	logout() {
+		this.loginService.logout();
+		this.init();
+	}
+
+	private init() {
+		this.loggedInUser = this.loginService.getLoggedInUser();
+		console.log(this.loggedInUser, "fetched");
+	}
 }
